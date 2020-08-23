0f9255a4ffb13b102f83ef180f72a995dc349010
Milanpreetsingh <smilanpreet7@gmail.com>
2020-08-23 18:36:56 +0530

    Updates

---
+++ precache-manifest.242d62317e31e92825d5d1b4f2579990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b981ca3e7c875639e3dc685a0ea622b6",
+    "url": "/budget_deploy/index.html"
+  },
+  {
+    "revision": "6faec90f99434aa9fbd7",
+    "url": "/budget_deploy/static/css/main.4e61f1da.chunk.css"
+  },
+  {
+    "revision": "767bc98512562b98a3cc",
+    "url": "/budget_deploy/static/js/2.eea6971c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/budget_deploy/static/js/2.eea6971c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6faec90f99434aa9fbd7",
+    "url": "/budget_deploy/static/js/main.be60f19d.chunk.js"
+  },
+  {
+    "revision": "13b314b8b6d951341559",
+    "url": "/budget_deploy/static/js/runtime-main.97250669.js"
+  }
+]);
